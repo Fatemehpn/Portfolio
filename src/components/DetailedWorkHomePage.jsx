bad68aa8e935ed1d073845aad8beab5406dbde9c
fatima <f.pournassari@gmail.com>
2024-10-14 00:16:13 -0700

    Styled the tabs styled the home work section styled single work page

--- src/components/DetailedWorkHomePage.jsx
+++ src/components/DetailedWorkHomePage.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
+import {useState, useEffect, useRef} from 'react'
 import { restBase } from '../utilities/utilities'
 import { rightArrow } from '../utilities/svg';
 import { Link } from 'react-router-dom';
+import { motion, useScroll,useTransform, MotionValue } from 'framer-motion';
 
 function DetailedWorkHomePage() {
   const restPathPosts = restBase + 'fp-work/?_embed=true&acf_format=standard';
@@ -23,40 +24,44 @@ function DetailedWorkHomePage() {
     fetchData()
   },[restPathPosts])
 
+
+
   return (
-   <section className='home-work-section section' id='work-section'>
-    { isLoaded?
-      <section id={`post-${restDataWork.id}`}> 
-        <h2>Featured Works</h2>
-        {
-          restDataWork.map(post=>
-            <article key={post.id}>
-              <div className='home-page-single-work'>
-                <h3>{post.acf.work_title}</h3>
+    <section className='home-work-section section' id='work-section'>
+     { isLoaded?
+       <section id={`post-${restDataWork.id}`}> 
+         <h2>Featured Works</h2>
+         {
+           restDataWork.map(post=>
+             <article key={post.id}>
+               <div className='home-page-single-work'  >
+                 <h3>{post.acf.work_title}</h3>
+                 <div className='top-skills'>
+                   {(post.acf.tools).map(tool=>
+                     <p>{tool}</p>
+                   )}
+                 </div>
+ 
+                 <p className='work-short-overview'>{post.acf.work_short_description}</p>
+                 <Link className='learn-more-button' to={`singleworkpage/${post.slug}`}>
+                   <p>Learn More</p>
+                   {rightArrow}
+                 </Link>
+               </div>
+               <div className='image-container' >
+                 <img src={post.acf.work_image.url} alt={post.title.rendered}/>
+               </div>
+             </article>
+           )
+         }
+ 
+       </section>
+       :
+       <p>Nothing is Loaded</p>
+     }
+    </section>
+   )
+  }
 
-                {(post.acf.tools).map(tool=>
-                  <span>{tool}</span>
-                )}
-
-                <p>{post.acf.work_short_description}</p>
-                <Link className='learn-more-button' to={`singleworkpage/${post.slug}`}>
-                  <p>Learn More</p>
-                  {rightArrow}
-                </Link>
-              </div>
-              <div className='image-container'>
-                <img src={post.acf.work_image.url} alt={post.title.rendered}/>
-              </div>
-            </article>
-          )
-        }
-
-      </section>
-      :
-      <p>Nothing is Loaded</p>
-    }
-   </section>
-  )
-}
 
 export default DetailedWorkHomePage

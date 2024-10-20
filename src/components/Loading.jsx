fbebe34f2cda3732132995b36df89c85cb34493c
fatima <f.pournassari@gmail.com>
2024-10-20 12:26:49 -0700

    Added loading component, styled it and fixed the loading state

---
+++ src/components/Loading.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div className='loading-wrapper'>
+      <img src="/logo.svg" alt="logo" className='loading-logo'/>
+    </div>
+  )
+}
+
+export default Loading

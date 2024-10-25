f3623c10deb7b351fd07a294b138b78b777166db
fatima <f.pournassari@gmail.com>
2024-10-25 13:03:47 -0700

    Fixed all the errors in console

--- src/components/LandingSectionHome.jsx
+++ src/components/LandingSectionHome.jsx
@@ -17,7 +17,6 @@ function LandingSectionHome() {
       if(response_items.ok){
         const landingData = await response_items.json();
         setDataLanding(landingData);
-        console.log(restDataLanding);
         setIsLoaded(true);
       }else{
         setIsLoaded(false);
@@ -40,7 +39,7 @@ function LandingSectionHome() {
           <motion.svg 
             animate={{ y:[0,-3,0,3,0] }}
             transition={{ duration:0.9 ,ease:'easeInOut', repeat:Infinity}}
-            enable-background="new 0 0 26 26" height="26px" id="Layer_1" version="1.1" viewBox="0 0 26 26" width="26px" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g><polygon fill="#231F20" points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon fill="#231F20" points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502  "/></g></motion.svg>
+            enableBackground="new 0 0 26 26" height="26px" id="Layer_1" version="1.1" viewBox="0 0 26 26" width="26px" xmlSpace="preserve" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"><g><polygon fill="#231F20" points="0.046,2.582 2.13,0.498 12.967,11.334 23.803,0.498 25.887,2.582 12.967,15.502  "/><polygon fill="#231F20" points="0.046,13.582 2.13,11.498 12.967,22.334 23.803,11.498 25.887,13.582 12.967,26.502  "/></g></motion.svg>
         </Link>
       </section>
       :
